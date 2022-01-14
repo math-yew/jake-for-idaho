23da56d9acaeb5d0355b24109e19b9d3f18eb239
math-yew <math.u@hotmail.com>
2022-01-14 14:15:47 -0700

    more in json

---
+++ js/words.js
@@ -0,0 +1,23 @@
+// var words = JSON.parse(words);
+// var words = words;
+
+let changes = document.querySelectorAll('[change]');
+let originalWords = {};
+for(var i=0; i<changes.length; i++){
+  let c = changes[i];
+  let val = c.innerHTML;
+  console.log("c: " + c.innerHTML);
+  let attribute = c.getAttribute('change')
+  console.log("a: " + attribute);
+  originalWords[attribute] = val;
+}
+
+console.log("words = " + JSON.stringify(originalWords));
+
+for(w in words){
+  let element = document.querySelectorAll('[change="' + w + '"]');
+  if(!!element[0]){
+    element[0].innerHTML = words[w];
+    console.log("= " + element[0].innerHTML);
+  }
+}
