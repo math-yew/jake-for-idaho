9962444f67d53b3e929f48c17c725d364245e35c
math-yew <math.u@hotmail.com>
2022-01-25 17:12:48 -0700

    donation additional text

--- js/words.js
+++ js/words.js
@@ -6,9 +6,9 @@ let originalWords = {};
 for(var i=0; i<changes.length; i++){
   let c = changes[i];
   let val = c.innerHTML;
-  console.log("c: " + c.innerHTML);
+  // console.log("c: " + c.innerHTML);
   let attribute = c.getAttribute('change')
-  console.log("a: " + attribute);
+  // console.log("a: " + attribute);
   originalWords[attribute] = val;
 }
 
@@ -18,6 +18,6 @@ for(w in words){
   let element = document.querySelectorAll('[change="' + w + '"]');
   if(!!element[0]){
     element[0].innerHTML = words[w];
-    console.log("= " + element[0].innerHTML);
+    // console.log("= " + element[0].innerHTML);
   }
 }
